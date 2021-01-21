83e57db98600c5cb158b34499e6ba4cbab0ad1b8
Ryan McErlean <rmcerlean@dom01.intranet>
2021-01-21 09:28:34 +0100

    new version

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-//You must add your own API key here from Clarifai.
+
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE'
+ apiKey: '34ef79895b6d421f9f61bf53a4b5c332'
 });
 
 const particlesOptions = {
   particles: {
     number: {
-      value: 30,
+      value: 150,
       density: {
         enable: true,
         value_area: 800
@@ -81,15 +81,7 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
